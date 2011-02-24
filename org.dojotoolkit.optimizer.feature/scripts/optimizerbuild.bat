ece4755a01de4d6f13e65739220079c1cba0ca5f
Richard Backhouse <richardabackhouse@gmail.com>
2011-02-24 12:02:55 -0500

    Update build scripts to point to correct workspace directory

--- org.dojotoolkit.optimizer.feature/scripts/optimizerbuild.bat
+++ org.dojotoolkit.optimizer.feature/scripts/optimizerbuild.bat
@@ -1,5 +1,5 @@
 @echo off
 
-apache-ant-1.8.2\bin\ant -lib org.ant4eclipse_1.0.0.M4\org.ant4eclipse_1.0.0.M4.jar -lib org.ant4eclipse_1.0.0.M4\libs -Dbasedir=%CD% -f ..\workspace\org.dojotoolkit.optimizer.feature\scripts\optimizerbuild.xml
+apache-ant-1.8.2\bin\ant -lib org.ant4eclipse_1.0.0.M4\org.ant4eclipse_1.0.0.M4.jar -lib org.ant4eclipse_1.0.0.M4\libs -Dbasedir=%CD% -f ..\zazl\org.dojotoolkit.optimizer.feature\scripts\optimizerbuild.xml
 
 
