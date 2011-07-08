8e5026780557c1543f63f9d17d32db036eee0156
Richard Backhouse <richardabackhouse@gmail.com>
2011-07-08 11:10:44 -0400

    Add copyright

--- org.dojotoolkit.optimizer.tests/src/org/dojotoolkit/optimizer/tests/RhinoAMDCircularDependencyTest.java
+++ org.dojotoolkit.optimizer.tests/src/org/dojotoolkit/optimizer/tests/RhinoAMDCircularDependencyTest.java
@@ -1,3 +1,8 @@
+/*
+    Copyright (c) 2004-2011, The Dojo Foundation All Rights Reserved.
+    Available via Academic Free License >= 2.1 OR the modified BSD license.
+    see: http://dojotoolkit.org/license for details
+*/
 package org.dojotoolkit.optimizer.tests;
 
 import org.dojotoolkit.optimizer.amd.rhino.AMDJSOptimizerFactory;
