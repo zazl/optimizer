cc419aae55710120df4754d9675bfd1e21768187
Richard Backhouse <richardabackhouse@gmail.com>
2012-08-28 16:20:08 -0400

    Another sample needing to add module config

--- org.dojotoolkit.optimizer.samples/amdtest/calendar-main.js
+++ org.dojotoolkit.optimizer.samples/amdtest/calendar-main.js
@@ -1,5 +1,6 @@
 zazl({
-	packages: [{name: 'dojo'}, {name: 'dijit'}, {name: 'dojox'}]
+	packages: [{name: 'dojo'}, {name: 'dijit'}, {name: 'dojox'}],
+    config: { "amdtest/Calendar": {myconfigval: "My Config Value"}}
 }, 
 ["amdtest/Calendar"], 
 function(calendar) {
