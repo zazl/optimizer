ee7bef0ab01ec5b093ea8186c6f3a26b8b65bfd9
Richard Backhouse <richardabackhouse@gmail.com>
2012-11-05 15:32:01 -0500

    Add a couple of different "define" signatures to improve test coverage

--- org.dojotoolkit.optimizer.samples/amdtest/Calendar.js
+++ org.dojotoolkit.optimizer.samples/amdtest/Calendar.js
@@ -1,10 +1,13 @@
+//"function"===typeof define && define([
+//(typeof define === "undefined" ? function(deps, def) { def(); } : define)([
 define([
 	'dojo',
 	'dijit/Calendar',
 	'module',
 	'dojo/domReady!'
 ], function (dojo, Calendar, module) {
-    var calendar = new Calendar({}, dojo.byId("calendarNode"));
-    console.log(module.config().myconfigval);
-    return calendar;
+	    var calendar = new Calendar({}, dojo.byId("calendarNode"));
+	    console.log(module.config().myconfigval);
+	    return calendar;
+	
 });
